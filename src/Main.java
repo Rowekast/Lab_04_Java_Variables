c6713411661b43d9e5e881fd8c1b892757fe6ba4
scott <rowekast@mail.uc.edu>
2025-09-19 16:37:18 -0400

    init

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,49 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    int intoperandA = 1543;
+    int intoperandB = 2545;
+    int intsum = intoperandA + intoperandB;
+    int intproduct = intoperandA * intoperandB;
+    int intdifference = intoperandA - intoperandB;
+    int intquotient = intoperandA / intoperandB;
+    int intmodulo = intoperandA % intoperandB;
+
+    System.out.println("The sum of " + intoperandA + " and " + intoperandB + " is: " + intsum);
+    System.out.println("The product of " + intoperandA + " and " + intoperandB + " is: " + intproduct);
+    System.out.println("The difference of " + intoperandA + " and " + intoperandB + " is: " + intdifference);
+    System.out.println("The quotient of " + intoperandA + " and " + intoperandB + " is: " + intquotient);
+    System.out.println("The modulo of " + intoperandA + " and " + intoperandB + " is: " + intmodulo);
+
+    double doubleoperandA = 1.2;
+    double doubleoperandB = 2.3;
+    double doublesum = doubleoperandA + doubleoperandB;
+    double doubleproduct = doubleoperandA * doubleoperandB;
+    double doubledifference = doubleoperandA - doubleoperandB;
+    double doublequotient = doubleoperandA / doubleoperandB;
+    double doublemodulo = doubleoperandA % doubleoperandB;
+
+    System.out.println("The sum of " + doubleoperandA + " and " + doubleoperandB + " is: " + doublesum);
+    System.out.println("The product of " + doubleoperandA + " and " + doubleoperandB + " is: " + doubleproduct);
+    System.out.println("The difference of " + doubleoperandA + " and " + doubleoperandB + " is: " + doubledifference);
+    System.out.println("The quotient of " + doubleoperandA + " and " + doubleoperandB + " is: " + doublequotient);
+    System.out.println("The modulo of " + doubleoperandA + " and " + doubleoperandB + " is: " + doublemodulo);
+
+
+    double myLunchCost = 0.0;
+    int kidsInFamily = 1;
+    boolean isRaining = false;
+    double gasCost = 3.5;
+    double favNum = 3.0;
+    double shoeSize = 10.0;
+    String fullName = "Scott Rowekamp";
+
+    System.out.println("My lunch cost: " + myLunchCost);
+    System.out.println("There are " + kidsInFamily + " kid(s) in my family");
+    System.out.println("Is it raining? Answer: " + isRaining);
+    System.out.println("The gas cost is: " + gasCost);
+    System.out.println("My favorite number is: " + favNum);
+    System.out.println("My shoe size is: " + shoeSize);
+    System.out.println("My full name is: " + fullName);
 }
